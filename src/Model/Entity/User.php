8f88baa77b8e772bb4a7d839ee856c2cf53e0765
somratcste <somratcste@gmail.com>
2017-04-25 22:30:48 +0530

    crete CRUD

---
+++ src/Model/Entity/User.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\ORM\Entity;
+
+/**
+ * User Entity
+ *
+ * @property int $id
+ * @property string $email
+ * @property string $password
+ * @property \Cake\I18n\Time $created
+ * @property \Cake\I18n\Time $modified
+ *
+ * @property \App\Model\Entity\Bookmark[] $bookmarks
+ */
+class User extends Entity
+{
+
+    public function _setPassword($value){
+        $hasher = new DefaultPasswordHasher();
+        return $hasher->hash($value);
+    }
+    protected $_accessible = [
+        '*' => true,
+        'id' => false
+    ];
+
+    /**
+     * Fields that are excluded from JSON versions of the entity.
+     *
+     * @var array
+     */
+    protected $_hidden = [
+        'password'
+    ];
+}
