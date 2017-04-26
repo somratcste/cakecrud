37febd866b2096e39a7a6b23dd6476a206f796f2
somratcste <somratcste@gmail.com>
2017-04-26 07:14:08 +0530

    add authorization

--- src/Controller/BookmarksController.php
+++ src/Controller/BookmarksController.php
@@ -126,4 +126,24 @@ class BookmarksController extends AppController
         ]);
 
     }
+
+    public function isAuthorized($user)
+    {
+        $action = $this->request->getParam('action');
+// The add and index actions are always allowed.
+        if (in_array($action, ['index', 'add', 'tags'])) {
+            return true;
+        }
+// All other actions require an id.
+        if (!$this->request->getParam('pass.0')) {
+            return false;
+        }
+// Check that the bookmark belongs to the current user.
+        $id = $this->request->getParam('pass.0');
+        $bookmark = $this->Bookmarks->get($id);
+        if ($bookmark->user_id == $user['id']) {
+            return true;
+        }
+        return parent::isAuthorized($user);
+    }
 }
--- src/Template/Layout/default.ctp
+++ src/Template/Layout/default.ctp
@@ -34,6 +34,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->fetch('script') ?>
 </head>
 <body>
+    <?= $this->Flash->render() ?>
     <nav class="top-bar expanded" data-topbar role="navigation">
         <ul class="title-area large-3 medium-4 columns">
             <li class="name">
